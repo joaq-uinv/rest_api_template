abfbc98f5afbca2d6202449122e463f345c61371
Joaquín Villanueva <villanuevajoaquin94@gmail.com>
2021-05-20 19:47:44 -0300

    method to handle errors with a status of 404 and 500 added in the express server class. comments added in the auth's services file

--- src/loaders/server/Server.js
+++ src/loaders/server/Server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const morgan = require("morgan");
 const swaggerUI = require("swagger-ui-express");
 const config = require("../../config/config");
+const logger = require("../logger/logger");
 const userRoutes = require("../../routes/userRoutes");
 const authRoute = require("../../routes/authRoutes");
 const swaggerJSON = require("../swagger/swagger.json");
-const logger = require("../logger/logger");
 
 class Server {
   constructor() {
@@ -15,6 +15,8 @@ class Server {
     this._middlewares();
     this._routes();
     this._swaggerConfig();
+    this._notFound();
+    this._errorHandler();
   }
 
   _middlewares() {
@@ -27,6 +29,41 @@ class Server {
     this.app.use("/", authRoute); //use the routes created in the auth route file in the /api/v1 route
   }
 
+  //external error
+  _notFound() {
+    this.app.use((req, res, next) => {
+      const err = new Error("Not Found");
+      //create the attr code the the err variable
+      err.code = 404;
+      next(err);
+    });
+  }
+
+  //internal server error
+  _errorHandler() {
+    /**
+     * @param {express.ErrorRequestHandler} err
+     */
+    this.app.use((err, req, res, next) => {
+      //define the code of the error
+      const code = err.code || 500;
+
+      logger.error(`${code} - ${err.message}`);
+      logger.error(err.stack);
+
+      //define the structure of the error
+      const body = {
+        error: {
+          code,
+          message: err.message,
+          detail: err.data,
+        },
+      };
+      //parse the error
+      res.status(code).json(body);
+    });
+  }
+
   _swaggerConfig() {
     this.app.use(
       config.swagger.path,
@@ -35,14 +72,12 @@ class Server {
     );
   }
 
-  //!ADD ERROR HANDLERS
-
   async start() {
     //app will listen on port set in config file
     this.app.listen(this.port, (error) => {
       //if there's an error, the app will shut down
       if (error) {
-        logger.info(error);
+        logger.error(error);
         process.exit(1);
         return;
       }
